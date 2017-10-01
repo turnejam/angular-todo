f41116125019dda8b74eda7ac4839e4829ce1b2b
Will Turner <turnejam@gmail.com>
2017-10-01 10:21:50 -0400

    Checkpoint 3 Angular

---
+++ app.js
@@ -0,0 +1,16 @@
+angular
+  .module('todo', [])
+  .controller('TodoController', TodoController);
+
+function TodoController() {
+  this.tasks = ['Walk the cat', 'Eat second breakfast', 'Watch Rocky cutscenes on youtube since too tired to workout'];
+
+  this.createTask = () => {
+    this.tasks.push(this.taskName);
+    this.taskName = "";
+  };
+
+  this.deleteTask = (index) => {
+    this.tasks.splice(index, 1);
+  };
+}
